2eda560449143a911aae8b8bb908842bce05177a
psbin2017 <123qkrtnqls@gmail.com>
2021-10-18 20:14:50 +0900

    ISS-004 테스트 프로파일이 프로젝트 프로파일을 읽지 않는 부분
    테스트 코드는 격리된 프로파일로 작성한다.

---
+++ src/test/java/configuration/SystemPropertyActiveProfileResolver.java
@@ -0,0 +1,16 @@
+package configuration;
+
+import org.springframework.test.context.ActiveProfilesResolver;
+import org.springframework.test.context.support.DefaultActiveProfilesResolver;
+
+public class SystemPropertyActiveProfileResolver implements ActiveProfilesResolver {
+    private final DefaultActiveProfilesResolver defaultActiveProfilesResolver = new DefaultActiveProfilesResolver();
+
+    @Override
+    public String[] resolve(Class<?> testClass) {
+        final String springProfileKey = "spring.profiles.active";
+
+        return System.getProperties().containsKey(springProfileKey) ?
+                System.getProperty(springProfileKey).split("\\s*,\\s*") : defaultActiveProfilesResolver.resolve(testClass);
+    }
+}
